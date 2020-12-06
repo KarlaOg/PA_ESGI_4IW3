ecbaac281df78347ef9e5fceeed0b5fd64f6dc2a
Yanis FENICHE <yanisfeniche@hotmail.fr>
2020-12-06 20:29:27 +0100

    ajout du menu, de la route de connexion et inscription, dépendance bootstrap et fontawesome

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/connexion")
+ */
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/", name="connexion")
+     */
+    public function index(): Response
+    {
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
