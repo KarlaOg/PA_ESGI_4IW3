27ed7b3779923a095e679a23cba1d8978a0897ff
Yanis FENICHE <yanisfeniche@hotmail.fr>
2021-01-02 12:18:59 +0100

    ajout de l'édition du profil utilisateur

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\EditProfileType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +20,63 @@ class UsersController extends AbstractController
         return $this->render('users/index.html.twig', [
             'controller_name' => 'UsersController',
         ]);
+    }
+
+    /**
+     * @Route("/users/data", name="users_data")
+     */
+    public function usersData()
+    {
+        return $this->render('users/data.html.twig');
+    }
+
+    /**
+     * @Route("/users/profil/modifier", name="users_profil_modifier")
+     */
+    public function editProfile(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/editprofile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/users/pass/modifier", name="users_pass_modifier")
+     */
+    public function editPass(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            // On vérifie si les 2 mots de passe sont identiques
+            if ($request->request->get('pass') == $request->request->get('pass2')) {
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+                //return $this->redirectToRoute('users');
+            } else {
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+        }
+
+        return $this->render('users/editpass.html.twig');
     }
 }
