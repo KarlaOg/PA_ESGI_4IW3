906f747b02e3c70705603ed4e94d96e4c9df6ed2
Emeline <emeline.garo@supinternet.fr>
2021-01-21 21:20:14 +0100

    add choice

--- app/src/Form/EditProfileType.php
+++ app/src/Form/EditProfileType.php
@@ -6,16 +6,34 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EditProfileType extends AbstractType
 {
+
+    const YOUTUBER = 'Youtuber';
+    const INFLUENCER = 'Influencer';
+    const GAMER = 'Gamer';
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
             ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => array(
+                    'Youtuber' => self::YOUTUBER,
+                    'Influencer' => self::INFLUENCER,
+                    'Gamer' => self::GAMER,
+                ),
+                'multiple'  => true,
+                'expanded' => true,
+            ])
             ->add('Valider', SubmitType::class);
     }
 
