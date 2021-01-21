dac0be002b7f606c7f1234a496aea24d024ef1dc
moussia <moussiamottal@gmail.com>
2021-01-21 11:17:43 +0100

    ajout template

---
+++ .history/app/src/Controller/UserProfilController_20210121102952.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+
+class UserProfilController extends AbstractController
+{
+    /**
+     * @Route("/user/profil/{$id}", name="show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        
+        return $this->render('user_profil/index.html.twig', [
+            'controller_name' => 'UserProfilController',
+            'id' => $id
+        ]);
+    }
+
+}
