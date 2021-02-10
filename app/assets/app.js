f92cd9093fda831dd493cab3d6d3e3afd21b905b
moussia <moussiamottal@gmail.com>
2021-02-10 11:00:21 +0100

    changement de js

--- app/assets/app.js
+++ app/assets/app.js
@@ -6,3 +6,31 @@ import 'material-icons';
 
 var elems = document.querySelectorAll('select');
 var instances = M.FormSelect.init(elems);
+
+$('select').change(function() {
+    var donnees = {
+        'searchSelector': $(this).children('option:selected').val()
+    };
+    $.ajax({
+        url: '/search/keyworddata',
+        type: 'POST',
+        dataType: 'json',
+        data: donnees,
+        async: true,
+        success: function(data, status) {
+            var tab = [];
+            for (var i = 0; i < data.length; i++) {
+                tab[i] = data[i].firstname + ' ' + data[i].lastname;
+            }
+            autocomplete(document.getElementById("keywords"), tab);
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            console.log('Ajax request failed. ' + errorThrown);
+        }
+    });
+});
+$('#keywords').keyup(function() {
+    var keywords = $('#keywords').val();
+    var tagselect = $('#tag-select').val();
+    $('#asb').attr("href", "/search/data?keywords=" + keywords + "&searchfilter=" + tagselect);
+});
