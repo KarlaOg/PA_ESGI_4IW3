449e2f24dd2908e77f1145e437aa2456b02f501a
Karla Og <karla.og@supinternet.fr>
2021-03-05 20:23:01 +0100

    merged conflitcs and navbar change

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,7 +1,4 @@
 import './styles/app.scss';
-import 'materialize-css';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'material-icons';
 
 
 var elems = document.querySelectorAll('select');
