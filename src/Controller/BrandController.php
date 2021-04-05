da1359e357b281dd8637afaf205ed488433d9d4f
kumatata <coumba.camara75@gmail.com>
2021-04-06 00:33:04 +0200

    recreation of feature user page : influencers list + profil page

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BrandRepository ; 
 
 class BrandController extends AbstractController
 {
@@ -17,4 +18,14 @@ class BrandController extends AbstractController
             'controller_name' => 'BrandController',
         ]);
     }
+
+      /**
+     * @Route("/all_brand", name="all_brand")
+     */
+    public function brands_list(BrandRepository $brandRepository) : Response
+    {
+        return $this->render('brand/list.html.twig', [
+            'brands' => $brandRepository->findBy([], ['position' => 'ASC'])
+        ]);
+    }  
 }
--- src/Controller/InfluencerController.php
+++ src/Controller/InfluencerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\InfluencerRepository ; 
 
 class InfluencerController extends AbstractController
 {
@@ -15,6 +16,26 @@ class InfluencerController extends AbstractController
     {
         return $this->render('influencer/index.html.twig', [
             'controller_name' => 'InfluencerController',
+        ]);
+    }
+
+  /**
+     * @Route("/all_influencers", name="all_influencers")
+     */
+    public function influencers_list(InfluencerRepository $influencerRepository) : Response
+    {
+        return $this->render('influencer/list.html.twig', [
+            'influencers' => $influencerRepository->findAll()
+        ]);
+    }
+    
+        /**
+     * @Route("influencer/show/{slug}", name="influencer_show", methods={"GET"})
+     */
+    public function show(Influencer $influencer): Response
+    {
+        return $this->render('influencer/show.html.twig', [
+            'influencer' => $influencer
         ]);
     }
 }
