5ee8167744b474da0ce96e299f6fa94f114036b8
Emeline GARO <emeline.garo@supinternet.fr>
2021-06-21 23:12:57 +0200

    remove useless file

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -12,11 +12,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\Mercure\PublisherInterface;
-use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Mercure\HubInterface;
 
 class MessageController extends AbstractController
 {
