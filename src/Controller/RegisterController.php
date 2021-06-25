45089c67b1da7cdcd256b83dd0886cf9d419d293
Mohand AIT AMARA <mohand.aitamara@faurecia.com>
2021-06-26 00:15:12 +0200

    modif du controler Register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,23 +4,25 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Brand;
+use App\Form\BrandType;
 use App\Entity\Influencer;
 use App\Form\RegisterType;
-use App\Form\BrandType;
 use App\Form\InfluencerType;
+use App\Repository\BrandRepository;
+use App\Repository\InfluencerRepository;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Security\LoginFormAuthenticator;
 
-use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 /**
@@ -44,8 +46,6 @@ class RegisterController extends AbstractController
     public function createAction(Request $request, EntityManagerInterface $manager, NotifierInterface $notifier, LoginFormAuthenticator $login, GuardAuthenticatorHandler $guard)
     {
         $user = new User();
-        $brand = new Brand();
-        $influencer = new Influencer();
         $form = $this->createForm(RegisterType::class, $user);
         $form->handleRequest($request);
         $userLogged = $this->getUser();
@@ -55,24 +55,24 @@ class RegisterController extends AbstractController
         }
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
             $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
-            $em = $this->getDoctrine()->getManager();
 
             if (array_search("ROLE_INFLUENCEUR", $user->getRoles()) !== false) {
+                $influencer = new Influencer();
                 $influencer->setUser($user);
-                
+                $em = $this->getDoctrine()->getManager();
                 $em->persist($influencer);
-
             } else if (array_search("ROLE_MARQUE", $user->getRoles()) !== false) {
+                $brand = new Brand();
                 $brand->setUser($user);
 
+                $em = $this->getDoctrine()->getManager();
                 $em->persist($brand);
             }
-           
+
             $userEmail = $user->getEmail();
             $notification = (new Notification('Confirmation d\'inscription', ['email']))
-                ->content('Bienvenue '. $user->getFirstname() .' '. $user->getLastname() . ' chez LIKEY et Merci pour votre confiance.');
+                ->content('Bienvenue ' . $user->getLastname() . ' chez LIKEY et Merci pour votre confiance.');
 
             // user recoit le mail
             $recipient = new Recipient(
@@ -82,17 +82,65 @@ class RegisterController extends AbstractController
             $notifier->send($notification, $recipient);
 
 
+            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
             $this->addFlash("success", "Inscription réussie !");
-            //'main' is your main Firewall. You can check it in config/packages/security.yaml
-            return $guard->authenticateUserAndHandleSuccess($user, $request, $login, 'main');
+
+            $guard->authenticateUserAndHandleSuccess($user, $request, $login, 'main');
+
+            return $this->redirectToRoute('register_complete');
         }
-        
+
         // afficher le formulaire s'il n'est pas déjà rempli
         return $this->render('register/index.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
+    }
+
+
+    /**
+     * @Route("/complete", name="complete")
+     */
+    public function complete(Request $request, EntityManagerInterface $em, InfluencerRepository $influencerRepository, BrandRepository $brandRepository)
+    {
+        $user = $this->getUser();
+
+
+        $influcerInfos = $influencerRepository->findOneBy(['user' => $user]);
+        $brandInfos = $brandRepository->findOneBy(['user' => $user]);
+        if ($user->getRoles() == ["ROLE_INFLUENCEUR"] ||   $user->getRoles()[0] == "ROLE_INFLUENCEUR") {
+            $form = $this->createForm(InfluencerType::class, $influcerInfos);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->flush();
+
+                return $this->redirectToRoute('users_data');
+            }
+
+            $formView = $form->createView();
+
+            return $this->render('register/complete_influencer.html.twig', [
+                'formView' => $formView,
+            ]);
+        } else {
+            $form = $this->createForm(BrandType::class, $brandInfos);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->flush();
+
+                return $this->redirectToRoute('users_data');
+            }
+            $formView = $form->createView();
+
+            return $this->render('register/complete_brand.html.twig', [
+                'formView' => $formView,
+            ]);
+        }
+
+        return $this->redirectToRoute('users_data');
     }
 }
