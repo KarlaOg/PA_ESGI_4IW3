e853231bd5aa151a0b0603cc347e5d1530c124f7
Emeline GARO <emeline.garo@supinternet.fr>
2021-06-26 16:22:44 +0200

    fix

--- migrations/Version20210626141625.php
+++ migrations/Version20210626141625.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210626141123 extends AbstractMigration
+final class Version20210626141625 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,6 @@ final class Version20210626141123 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE chat');
         $this->addSql('ALTER TABLE notification CHANGE time time DATETIME NOT NULL');
         $this->addSql('ALTER TABLE offer CHANGE date_creation date_creation DATETIME NOT NULL, CHANGE date_start date_start DATETIME NOT NULL, CHANGE date_end date_end DATETIME NOT NULL');
         $this->addSql('ALTER TABLE payment CHANGE date_creation date_creation DATETIME NOT NULL');
@@ -30,9 +29,6 @@ final class Version20210626141123 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE chat (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, sender_id INT DEFAULT NULL, time DATETIME NOT NULL, message VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_659DF2AAF624B39D (sender_id), INDEX IDX_659DF2AA72F5A1AA (channel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE chat ADD CONSTRAINT FK_659DF2AA72F5A1AA FOREIGN KEY (channel_id) REFERENCES channel (id)');
-        $this->addSql('ALTER TABLE chat ADD CONSTRAINT FK_659DF2AAF624B39D FOREIGN KEY (sender_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE notification CHANGE time time DATETIME NOT NULL');
         $this->addSql('ALTER TABLE offer CHANGE date_creation date_creation DATETIME NOT NULL, CHANGE date_start date_start DATETIME NOT NULL, CHANGE date_end date_end DATETIME NOT NULL');
         $this->addSql('ALTER TABLE payment CHANGE date_creation date_creation DATETIME NOT NULL');
