e01ca30b10799451df5f6721becd733dc271bdee
Yanis FENICHE <yanisfeniche@hotmail.fr>
2021-07-02 22:30:25 +0200

    ajouter le nombre de réponse à un commentaire

--- src/Form/CommentsType.php
+++ src/Form/CommentsType.php
@@ -4,15 +4,11 @@ namespace App\Form;
 
 use App\Entity\Comments;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class CommentsType extends AbstractType
 {
