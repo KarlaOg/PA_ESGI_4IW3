99884fbf9462a8ace8c51b1edaf9447249311b4e
Emeline GARO <emeline.garo@supinternet.fr>
2021-07-04 21:47:56 +0200

    get back faker siret

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -48,7 +48,7 @@ class AppFixtures extends Fixture
         $influencer->setUser($admin)
             ->setDescription($faker->realText())
             ->setUsername($username)
-            ->setSiret(75152319200027)
+            ->setSiret($faker->numberBetween(10, 2000))
             ->setName($resName)
             ->setType(["Gamer", "Instagramer", "Blogueur"])
             ->setSocialNetwork([
@@ -88,7 +88,7 @@ class AppFixtures extends Fixture
             $influencer->setUser($faker->unique()->randomElement($users))
                 ->setDescription($faker->realText())
                 ->setUsername($username)
-                ->setSiret(75152319200027)
+                ->setSiret($faker->numberBetween(10, 2000))
                 ->setName($resName)
                 ->setType(["Gamer", "Instagramer", "Blogueur"])
                 ->setSocialNetwork([
@@ -129,7 +129,7 @@ class AppFixtures extends Fixture
             $brand->setUser($faker->unique()->randomElement($users))
                 ->setDescription($faker->realText())
                 ->setUsername($name)
-                ->setSiret(75152319200027)
+                ->setSiret($faker->numberBetween(10, 2000))
                 ->setName($resName)
                 ->setField([
                     "Agroalimentaire",
